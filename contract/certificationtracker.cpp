9af7e641208702d68f4215243f8f5cae1d8fc37a
eayllon1 <eayllon1@gmail.com>
2019-04-18 17:28:36 -0400

    Update certificationtracker.cpp

--- contract/certificationtracker.cpp
+++ contract/certificationtracker.cpp
@@ -2,7 +2,7 @@
 
 using namespace eosio;
 
-class [[eosio::contract("certificationtracker")]] certificationtracker : public eosio::contract {
+class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
   public:
     using contract::contract;
     addressbook(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
@@ -10,10 +10,10 @@ class [[eosio::contract("certificationtracker")]] certificationtracker : public 
     [[eosio::action]]
     void upsert(name user, std::string first_name, std::string last_name, std::string street, std::string city, std::string state) {
       require_auth( user );
-      address_index certifications(_code, _code.value);
-      auto iterator = certifications.find(user.value);
-      if( iterator == certifications.end() ) {
-        certifications.emplace(user, [&]( auto& row ) {
+      address_index addresses(_code, _code.value);
+      auto iterator = addresses.find(user.value);
+      if( iterator == addresses.end() ) {
+        addresses.emplace(user, [&]( auto& row ) {
          row.key = user;
          row.first_name = first_name;
          row.last_name = last_name;
@@ -23,7 +23,7 @@ class [[eosio::contract("certificationtracker")]] certificationtracker : public 
         });
       } else {
         std::string changes;
-        certifications.modify(iterator, user, [&]( auto& row ) {
+        addresses.modify(iterator, user, [&]( auto& row ) {
           row.key = user;
           row.first_name = first_name;
           row.last_name = last_name;
@@ -37,14 +37,14 @@ class [[eosio::contract("certificationtracker")]] certificationtracker : public 
     [[eosio::action]]
     void erase(name user) {
       require_auth(user);
-      address_index certifications(_self, _code.value);
+      address_index addresses(_self, _code.value);
       auto iterator = addresses.find(user.value);
       eosio_assert(iterator != addresses.end(), "Record does not exist");
-      addressbook.cpp.erase(iterator);
+      addresses.erase(iterator);
     }
 
   private:
-    struct [[eosio::table]] certification {
+    struct [[eosio::table]] person {
       name key;
       std::string first_name;
       std::string last_name;
@@ -53,7 +53,7 @@ class [[eosio::contract("certificationtracker")]] certificationtracker : public 
       std::string state;
       uint64_t primary_key() const { return key.value; }
     };
-    typedef eosio::multi_index<"record"_n, certification> certification_index;
+    typedef eosio::multi_index<"people"_n, person> address_index;
 };
 
-EOSIO_DISPATCH( certifications, (upsert)(erase))
+EOSIO_DISPATCH( addressbook, (upsert)(erase))
