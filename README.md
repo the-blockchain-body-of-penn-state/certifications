ce0af5653df707a697a43015668541e53f3de0ee
arudenius <jwu123456@gmail.com>
2019-04-18 17:44:49 -0400

    updated certificationtracker.cpp to store certifications data

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # certifications
+This is the repo for EOS powered certifications platform. The goal of this platform is to develop an open, trustless platform for certifications pooling and signing. 
+
+Nuclino Link: http://bit.ly/2Uxae02
--- contract/certificationtracker.cpp
+++ contract/certificationtracker.cpp
@@ -8,28 +8,24 @@ class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
     addressbook(name receiver, name code,  datastream<const char*> ds): contract(receiver, code, ds) {}
 
     [[eosio::action]]
-    void upsert(name user, std::string first_name, std::string last_name, std::string street, std::string city, std::string state) {
+    void upsert(name user, double score, std::string date, std::string field) {
       require_auth( user );
       address_index addresses(_code, _code.value);
       auto iterator = addresses.find(user.value);
       if( iterator == addresses.end() ) {
         addresses.emplace(user, [&]( auto& row ) {
          row.key = user;
-         row.first_name = first_name;
-         row.last_name = last_name;
-         row.street = street;
-         row.city = city;
-         row.state = state;
+         row.score = score;
+         row.date = date;
+         row.field = field;
         });
       } else {
         std::string changes;
         addresses.modify(iterator, user, [&]( auto& row ) {
           row.key = user;
-          row.first_name = first_name;
-          row.last_name = last_name;
-          row.street = street;
-          row.city = city;
-          row.state = state;
+          row.score = score;
+          row.date = date;
+          row.field = field;
         });
       }
     }
@@ -44,16 +40,14 @@ class [[eosio::contract("addressbook")]] addressbook : public eosio::contract {
     }
 
   private:
-    struct [[eosio::table]] person {
+    struct [[eosio::table]] certification{
       name key;
-      std::string first_name;
-      std::string last_name;
-      std::string street;
-      std::string city;
-      std::string state;
+      double score;
+      std::string date;
+      std::string field;
       uint64_t primary_key() const { return key.value; }
     };
-    typedef eosio::multi_index<"people"_n, person> address_index;
+    typedef eosio::multi_index<"certification"_n, certification> address_index;
 };
 
 EOSIO_DISPATCH( addressbook, (upsert)(erase))
